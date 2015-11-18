30bb6ad9c862be78ebfe61cda570a5d43f92c98e
Michel Meyer <michel.meyer@cargomedia.ch>
2015-11-18 11:55:26 +0100

    Fix formatting

--- cm/MousePositionSensorVRDevice.js
+++ cm/MousePositionSensorVRDevice.js
@@ -1,6 +1,6 @@
 var MouseKeyboardPositionSensorVRDevice = require('../src/mouse-keyboard-position-sensor-vr-device');
 
-function MousePositionSensorVRDevice(){
+function MousePositionSensorVRDevice() {
   MouseKeyboardPositionSensorVRDevice.call(this);
   this.deviceId = 'webvr-polyfill:mouse';
   this.deviceName = 'VR Position Device (webvr-polyfill:mouse)';
@@ -8,6 +8,7 @@ function MousePositionSensorVRDevice(){
 
 MousePositionSensorVRDevice.prototype = Object.create(MouseKeyboardPositionSensorVRDevice.prototype);
 MousePositionSensorVRDevice.prototype.constructor = MousePositionSensorVRDevice;
-MousePositionSensorVRDevice.prototype.onKeyDown_ = function() {};
+MousePositionSensorVRDevice.prototype.onKeyDown_ = function() {
+};
 
 module.exports = MousePositionSensorVRDevice;
