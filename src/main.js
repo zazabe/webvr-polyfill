9cc0476c4b949514b5b15f0ecb1e495c65a2865e
Michel Meyer <michel.meyer@cargomedia.ch>
2016-05-30 14:49:12 +0200

    Move default config into main.js and rebuild the polyfill

--- src/main.js
+++ src/main.js
@@ -63,7 +63,10 @@ window.WebVRConfig = Util.extend({
   // Dirty bindings include: gl.FRAMEBUFFER_BINDING, gl.CURRENT_PROGRAM,
   // gl.ARRAY_BUFFER_BINDING, gl.ELEMENT_ARRAY_BUFFER_BINDING,
   // and gl.TEXTURE_BINDING_2D for texture unit 0.
-  DIRTY_SUBMIT_FRAME_BINDINGS: false
+  DIRTY_SUBMIT_FRAME_BINDINGS: false,
+
+  // Enable the fetch of the online version of the DPDB
+  DPDB_FETCH_ONLINE: true
 }, window.WebVRConfig);
 
 if (!window.WebVRConfig.DEFER_INITIALIZATION) {
